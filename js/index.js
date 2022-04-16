bf9ffb6d2512a78460b8c0916a76e2a70f1a3b9e
yoyesu <yoyes@gmail.com>
2022-04-16 20:26:36 +0100

    better styling

--- js/index.js
+++ js/index.js
@@ -1,17 +1,9 @@
+let langIcon = document.querySelector('#lang-icon');
+let pagesIcon = document.querySelector('#pag-icon');
+let langDropdown = document.querySelector('#lang-drop');
+let pagesDropdown = document.querySelector('#pages-drop');
 
-
-
-// function to make the form work
-// let firstName = document.querySelector("#firstName");
-// let subBtn = document.querySelector("#form-btn");
-
-// if (subBtn) {
-//   subBtn.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     if (firstName.value == 0) {
-//       alert("Please, introduce your name to start playing.");
-//     } else {
-//       document.getElementById("welc-name").innerHTML = `Welcome, ${firstName.value}! Choose one of the games below and, let's play!`;
-//     }
-//   });
-// }
+langDropdown.setAttribute('class', 'hidden');
+pagesDropdown.setAttribute('class', 'hidden');
+langIcon.addEventListener('click', () => {langDropdown.classList.toggle('visible')})
+pagesIcon.addEventListener('click', () => {pagesDropdown.classList.toggle('visible')})
--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,19 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-/*----- constants -----*/
 let squaresArray = ['','','','','','','','',''];
+
+
 let grid = document.querySelector('.grid');
 let chosenToeId = [];
 let imgId = [];
+let turn = 'user';
+let takenId = [];
 
-/*----- app's state (variables) -----*/
-
-/*----- cached element references -----*/
-
-/*----- event listeners -----*/
-
-/*----- functions -----*/
-
+////////////creating the board///////////
 function ticTacBoard (){
   for (let i = 0; i < squaresArray.length; i++){
     let gridElements = document.createElement('div');
@@ -25,19 +21,45 @@ function ticTacBoard (){
     imgId.push(i);
     gridImg.setAttribute('src', 'img/tic-tac-toe/toe.png');
     gridImg.addEventListener('click', leaveMark);
-    console.log(imgId);
   }
 }
 
+////user's mark////
 function leaveMark(){
   let toes = document.querySelectorAll('img');
   let toesId = this.getAttribute('data-id');
   chosenToeId.push(toesId);
+  takenId.push(toesId);
   let chosenToe = chosenToeId[0];
   toes[chosenToe].setAttribute('src','img/tic-tac-toe/hammer.png');
-  console.log('chosen toe', chosenToe);
   chosenToeId = [];
+  setTimeout(computerTurn, 1000);
 }
+
+/////turns////////
+// function render() {
+//   squaresArray.forEach(function(mark, index){
+//   console.log(mark, index);
+//   });
+//   };
+// squaresArray[leaveMark] = turn;
+
+function computerTurn(){
+  var randomId = Math.floor((Math.random()*imgId.length));
+  let randomIdString = randomId.toString();
+  console.log(randomIdString);
+  console.log(typeof randomIdString);
+  let toes = document.querySelectorAll('img');
+  if (!takenId.includes(randomIdString)){
+    toes[randomId].setAttribute('src','img/tic-tac-toe/boots.png');
+    takenId.push(randomIdString);
+  } else {
+    var randomId = Math.floor((Math.random()*imgId.length));
+  }
+}
+
+console.log(takenId);
+
 
 ticTacBoard();
 
