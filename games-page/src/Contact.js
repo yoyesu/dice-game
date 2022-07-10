b658c8a76ebf165aa765055470e9e0ccef82d8c4
yoyesu <yoyes@gmail.com>
2022-07-10 20:38:22 +0100

    using gatsby

--- games-page/src/Contact.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Contact() {
-  return <h1>This is contact page</h1>;
-}
--- games-page/src/contact-index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Contact from "./contact-index";
-
-const rootContact = ReactDOM.createRoot(
-  document.getElementById("root-contact")
-);
-root.render(<Contact />);
